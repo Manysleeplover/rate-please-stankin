04db10bb760a8733180583b31ea1d5f544d938c3
romanovio <romanovio@vtb.ru>
2024-07-28 15:46:57 +0300

    Дописал конфигурацию безопасности

--- authorization-service/src/main/kotlin/ru/romanov/stankin/authorization_service/config/security/WebSecurityConfiguration.kt
+++ authorization-service/src/main/kotlin/ru/romanov/stankin/authorization_service/config/security/WebSecurityConfiguration.kt
@@ -2,8 +2,16 @@ package ru.romanov.stankin.authorization_service.config.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 
 
 @Configuration
@@ -14,17 +22,32 @@ class WebSecurityConfiguration {
         return BCryptPasswordEncoder()
     }
 
-//    @Bean
-//    fun filterChain(
-//        http: HttpSecurity,
-//        initialAuthenticationFilter: InitialAuthenticationFilter,
-//        jwtAuthorizationFilter: JwtAuthorizationFilter
-//    ): SecurityFilterChain {
-//
-//        http
-//            .authorizeHttpRequests { it.anyRequest().authenticated() }
-//            .httpBasic(Customizer.withDefaults())
-//        return http.build()
-//    }
+    @Bean
+    fun filterChain(
+        http: HttpSecurity,
+        initialAuthenticationFilter: InitialAuthenticationFilter,
+        jwtAuthorizationFilter: JwtAuthorizationFilter
+    ): SecurityFilterChain {
+
+        http.addFilterAt(initialAuthenticationFilter, BasicAuthenticationFilter::class.java)
+            .addFilterAt(jwtAuthorizationFilter, BasicAuthenticationFilter::class.java)
+
+//        http.authorizeHttpRequests {
+//            it
+//                .requestMatchers("/h2-console/**").permitAll()
+//                .anyRequest().authenticated()
+//        }
+
+        http.headers { headers -> headers.frameOptions(HeadersConfigurer<HttpSecurity>.FrameOptionsConfig::sameOrigin) }
+            .csrf(CsrfConfigurer<HttpSecurity>::disable)
+            .cors(CorsConfigurer<HttpSecurity>::disable);
+
+
+
+        http
+            .authorizeHttpRequests { it.anyRequest().authenticated() }
+            .httpBasic(Customizer.withDefaults())
+        return http.build()
+    }
 
 }
