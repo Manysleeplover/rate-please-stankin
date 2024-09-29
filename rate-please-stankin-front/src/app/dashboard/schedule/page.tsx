fbe1e74509a602ecd05455d76484e9330a9e0963
romanovio <romanovio@vtb.ru>
2024-09-29 16:20:55 +0300

    Добавил простой календарь

--- rate-please-stankin-front/src/app/dashboard/schedule/page.tsx
+++ rate-please-stankin-front/src/app/dashboard/schedule/page.tsx
@@ -1,23 +1,23 @@
 'use client'
 
-import {Calendar} from "@nextui-org/calendar";
-import {parseDate} from "@internationalized/date";
 import {Subject} from "@/app/ui/schedule/subject-layout";
 import scheduleMock from "@/app/lib/mocks/schedule-mock";
+import Calendar from 'react-calendar';
+import {useState} from "react";
+import 'react-calendar/dist/Calendar.css';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 
 export default function SchedulePage() {
+    const [value, onChange] = useState<Value>(new Date());
     const subjects = scheduleMock
     return (
         <main className="w-2/3 ml-auto mr-auto">
-            <div className="flex gap-x-4">
-
-                <Calendar aria-label="Date (No Selection)" className=""/>
-                <Calendar aria-label="Date (Uncontrolled)" defaultValue={parseDate("2020-02-03")}/>
-
-            </div>
-            {/*<CalendarDateRangeIcon width={100} height={100}/>*/}
-            <div className=" rounded-xl bg-gray-50 p-2 shadow-sm border-gray-200 border-2">
+            <Calendar onChange={onChange} value={value} />
+            <div className=" rounded-xl bg-gray-50 p-2 shadow-sm border-gray-200 ">
                 <div>
                     <Subject subjects={subjects}/>
                 </div>
