5585ca88b756e0cf23c62f87d2acff4f31079529
manysleeplover1 <manysleeplover@bk.ru>
2025-04-11 17:53:19 +0300

    форма создания тестового задания

--- rate-please-stankin-front/src/app/dashboard/create-test/page.tsx
+++ rate-please-stankin-front/src/app/dashboard/create-test/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { getSemesterSchedule } from "@/app/lib/api/schedule-api";
-import { SemesterSchedule } from "@/app/lib/api/ui-interfaces";
+import { getSemesterSchedule } from '@/app/lib/api/schedule-api';
+import { SemesterSchedule } from '@/app/lib/api/ui-interfaces';
+import SchedulePage from '@/app/dashboard/schedule/page';
 
 export default function SelectForm() {
     const [selectedOption, setSelectedOption] = useState<string>('');
     const [semesterScheduleItem, setSemesterScheduleItem] = useState<SemesterSchedule[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isLoadSchedule, setIsLoadSchedule] = useState<boolean>(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -15,58 +17,64 @@ export default function SelectForm() {
                 const resp = await getSemesterSchedule();
                 setSemesterScheduleItem(resp);
             } catch (error) {
-                console.error("Error fetching schedule:", error);
+                console.error('Error fetching schedule:', error);
             } finally {
                 setIsLoading(false);
             }
         };
 
         fetchData();
-    }, []); // Пустой массив зависимостей = выполнить только при монтировании
+    }, []);
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         alert(`Selected: ${selectedOption}`);
     };
 
+    const formatDate = (dateString: string | Date) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('ru-RU');
+    };
 
     if (isLoading) {
-        return <div className="p-4 max-w-md mx-auto">Загрузка...</div>;
+        return (
+            <div className="p-4 max-w-md mx-auto">
+                Загрузка...
+            </div>
+        );
     }
 
     return (
-        <div className="p-4 max-w-md mx-auto">
-            <h1 className="text-xl font-bold mb-4">Выберите группу</h1>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                    <label htmlFor="group-select" className="block mb-2">
-                        Группы:
-                    </label>
-                    <select
-                        id="group-select"
-                        value={selectedOption}
-                        onChange={(e) => setSelectedOption(e.target.value)}
-                        className="w-full p-2 border rounded"
-                        required
-                    >
-                        <option value="">-- Выберите --</option>
-                        {semesterScheduleItem.map((option) => (
-                            <option key={option.id} value={option.stgroup}>
-                                {option.stgroup} | {option.firstClassDate} - {option.lastClassDate}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <button
-                    type="submit"
-                    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                >
-                    Отправить
-                </button>
-            </form>
-            {selectedOption && (
-                <p className="mt-4">Вы выбрали: <strong>{selectedOption}</strong></p>
-            )}
-        </div>
+        <>
+            <div className="p-4 max-w-md mx-auto">
+                <h1 className="text-xl font-bold mb-4">Выберите группу</h1>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label htmlFor="group-select" className="block mb-2">
+                            Группы:
+                        </label>
+                        <select
+                            id="group-select"
+                            value={selectedOption}
+                            onChange={(e) => {
+                                setSelectedOption(e.target.value);
+                                setIsLoadSchedule(true);
+                            }}
+                            className="w-full p-2 border rounded"
+                            required
+                        >
+                            <option value="">-- Выберите --</option>
+                            {semesterScheduleItem.map((option) => (
+                                <option key={option.id} value={option.stgroup}>
+                                    {option.stgroup} | {formatDate(option.firstClassDate)} - {formatDate(option.lastClassDate)}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                </form>
+            </div>
+
+            {isLoadSchedule && <SchedulePage />}
+        </>
     );
 }
--- rate-please-stankin-front/src/app/dashboard/upload-schedule/page.tsx
+++ rate-please-stankin-front/src/app/dashboard/upload-schedule/page.tsx
@@ -67,7 +67,7 @@ const FileUploadComponent = () => {
 
                 <button
                     type="submit"
-                    className="w-full px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
+                    className="w-full px-4 py-2 bg-stankin_blue text-white rounded-md hover:bg-blue-700 transition-colors"
                 >
                     Загрузить
                 </button>
